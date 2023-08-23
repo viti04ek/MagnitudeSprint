065d15c890727e7636fa8c30b5871a7cba5e1a00
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-08-24 01:59:56 +0300

    Obstacle spawner
    ADDED:
    - Coin prefab
    - Enemy2 prefab
    - Enemy3 prefab
    - Enemy4 prefab
    - script ObstacleSpawner
    
    CHANGED:
    - BarbellObstacle prefab
    - Enemy1 prefab

---
+++ MagnitudeSprint/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public List<GameObject> Enemies;
+    public GameObject BarbellObstacle;
+    public GameObject Coin;
+    public GameObject SteakLoot;
+    public GameObject CarrotLoot;
+
+    private int _maxPossibleStrenght = 1;
+
+
+    private void OnEnable()
+    {
+        StartCoroutine("Spawn");
+    }
+
+
+    private IEnumerator Spawn()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            int rand = Random.Range(1, 6);
+            switch (rand)
+            {
+                case 1: SpawnOneEnemy(); break;
+                case 2: SpawnRowEnemies(); break;
+                case 3: SpawnBarbell(); break;
+                case 4: SpawnLoot(); break;
+                case 5: SpawnCoin(); break;
+            }
+
+            float randTime = Random.Range(0.5f, 0.9f);
+            yield return new WaitForSeconds(randTime);
+        }
+    }
+
+
+    private void SpawnOneEnemy()
+    {
+        int randNum = Random.Range(0, 4);
+        float ranCoord = Random.Range(-2.5f, 2.5f);
+        Instantiate(Enemies[randNum], new Vector3(ranCoord, transform.position.y, transform.position.z), Quaternion.Euler(0f, 180f, 0f));
+    }
+
+
+    private void SpawnRowEnemies()
+    {
+        if (_maxPossibleStrenght < 2)
+        {
+            SpawnLoot();
+            return;
+        }
+
+        int randNum1 = Random.Range(0, 4);
+        int randNum2 = Random.Range(0, 4);
+        int randNum3 = Random.Range(0, 4);
+        int randNum4 = Random.Range(0, 4);
+        int randNum5 = Random.Range(0, 4);
+        if (randNum1 > 0 && randNum2 > 0 && randNum3 > 0 && randNum4 > 0 && randNum5 > 0)
+            randNum3 = 0;
+
+        Instantiate(Enemies[randNum1], new Vector3(-2f, transform.position.y, transform.position.z), Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(Enemies[randNum2], new Vector3(-0.97f, transform.position.y, transform.position.z), Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(Enemies[randNum3], new Vector3(0.153f, transform.position.y, transform.position.z), Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(Enemies[randNum4], new Vector3(1.238f, transform.position.y, transform.position.z), Quaternion.Euler(0f, 180f, 0f));
+        Instantiate(Enemies[randNum5], new Vector3(2.376f, transform.position.y, transform.position.z), Quaternion.Euler(0f, 180f, 0f));
+
+        _maxPossibleStrenght--;
+    }
+
+
+    private void SpawnBarbell()
+    {
+        float ranCoord = Random.Range(-2.5f, 2.5f);
+        Instantiate(BarbellObstacle, new Vector3(ranCoord, 0.36f, transform.position.z), Quaternion.identity);
+    }
+
+
+    private void SpawnLoot()
+    {
+        if (_maxPossibleStrenght == 10)
+        {
+            SpawnRowEnemies();
+            return;
+        }
+
+        int rand = Random.Range(0, 2);
+        float randCoord1 = Random.Range(-2.5f, -0.5f);
+        float randCoord2 = Random.Range(0.5f, 2.5f);
+        if (rand == 0)
+        {
+            Instantiate(CarrotLoot, new Vector3(randCoord1, 0.5f, transform.position.z), Quaternion.identity);
+            Instantiate(SteakLoot, new Vector3(randCoord2, 0.5f, transform.position.z), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(CarrotLoot, new Vector3(randCoord2, 0.5f, transform.position.z), Quaternion.identity);
+            Instantiate(SteakLoot, new Vector3(randCoord1, 0.5f, transform.position.z), Quaternion.identity);
+        }
+
+        _maxPossibleStrenght++;
+    }
+
+
+    private void SpawnCoin()
+    {
+        float ranCoord = Random.Range(-2.5f, 2.5f);
+        Instantiate(Coin, new Vector3(ranCoord, 0.5f, transform.position.z), Quaternion.identity);
+    }
+}
