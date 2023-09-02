7c42879ccdab1438bdab8147e0e1a1d1dd6e0f05
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-09-03 00:00:20 +0300

    Change camera move
    CHANGED:
    - script CameraMovement

--- MagnitudeSprint/Assets/Scripts/CameraMovement.cs
+++ MagnitudeSprint/Assets/Scripts/CameraMovement.cs
@@ -11,12 +11,12 @@ public class CameraMovement : MonoBehaviour
     {
         if (Target.position.x < -1.6f)
         {
-            Vector3 target = new Vector3(-1f, transform.position.y, transform.position.z);
+            Vector3 target = new Vector3(-1.5f, transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, target, 0.1f);
         }
         else if (Target.position.x > 1.6f)
         {
-            Vector3 target = new Vector3(1f, transform.position.y, transform.position.z);
+            Vector3 target = new Vector3(1.5f, transform.position.y, transform.position.z);
             transform.position = Vector3.Lerp(transform.position, target, 0.1f);
         }
         else
