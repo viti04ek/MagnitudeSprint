d8b1546ca6723851d2d82c2753c0dfbde630de9f
viti04ek <99684929+viti04ek@users.noreply.github.com>
2023-09-15 15:42:57 +0300

    Skin item controll
    ADDED:
    - script SkinItemButton

---
+++ MagnitudeSprint/Assets/Scripts/Shop/SkinItemButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinItemButton : MonoBehaviour
+{
+    public int ID;
+    public GameObject Tick;
+    public Sprite UnlockedSprite;
+    public Image Image;
+
+
+    public void Click()
+    {
+
+    }
+}
